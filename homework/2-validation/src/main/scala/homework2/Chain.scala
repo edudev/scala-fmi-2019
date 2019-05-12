66316a0d2e11742aab2ad80be769e29548759971
Emil Dudev <emildudev@gmail.com>
2019-05-12 23:05:46 +0000

    Implement Chain

--- homework/2-validation/src/main/scala/homework2/Chain.scala
+++ homework/2-validation/src/main/scala/homework2/Chain.scala
@@ -1,34 +1,111 @@
 package homework2
 
+import scala.annotation.tailrec
+
 sealed trait Chain[+A] {
   def head: A
   def tail: Option[Chain[A]]
 
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = false
 
-  def +:[B >: A](front: B): Chain[B] = ???
+  def +:[B >: A](front: B): Chain[B] = Append(Singleton(front), this)
 
-  def :+[B >: A](back: B): Chain[B] = ???
+  def :+[B >: A](back: B): Chain[B] = Append(this, Singleton(back))
 
-  def ++[B >: A](right: Chain[B]): Chain[B] = ???
+  def ++[B >: A](right: Chain[B]): Chain[B] = Append(this, right)
 
-  def foldLeft[B](initial: B)(f: (B, A) => B): B = ???
+  /*
+  def foldLeft[B](initial: B)(f: (B, A) => B): B = this match {
+    case Singleton(element) => f(initial, element)
+    case Append(left, right) => right.foldLeft(left.foldLeft(initial)(f))(f)
+  }
+   */
 
+  def foldLeft[B](initial: B)(f: (B, A) => B): B = {
+    @tailrec
+    def helper(l: List[Chain[A]], r: List[Chain[A]], acc: B): B =
+      l match {
+        case Singleton(element) :: ls  => helper(ls, r, f(acc, element))
+        case Append(left, right) :: ls => helper(left :: ls, right :: r, acc)
+        case Nil =>
+          r match {
+            case Singleton(element) :: rs => helper(l, rs, f(acc, element))
+            case Append(left, right) :: rs =>
+              helper(left :: l, right :: rs, acc)
+            case Nil => acc
+          }
+      }
+
+    helper(List(this), Nil, initial)
+  }
+
+  /*
+  def foldRight[B](initial: B)(f: (A, B) => B): B = this match {
+    case Singleton(element) => f(element, initial)
+    case Append(left, right) => left.foldRight(right.foldRight(initial)(f))(f)
+  }
+   */
+
+  def reduceLeft[B >: A](f: (B, A) => B): B =
+    foldLeft(Option.empty[B])(
+      (acc, element) => Some(acc.fold(element: B)(f(_, element)))
+    ).get // safe, as there is always at least one element
+
+  /*
   def reduceLeft[B >: A](f: (B, A) => B): B = this.listify match {
     case Singleton(first) => first
     case Append(Singleton(first), rest) => rest.foldLeft(first: B)(f)
     case _ => sys.error("Unexpected listify format")
   }
+   */
 
-  def map[B](f: A => B): Chain[B] = ???
+  /*
+  def map[B](f: A => B): Chain[B] = this match {
+    case Singleton(element) => Singleton(f(element))
+    case Append(left, right) => Append(left map f, right map f)
+  }
+   */
 
-  def flatMap[B](f: A => Chain[B]): Chain[B] = ???
+  def map[B](f: A => B): Chain[B] =
+    foldLeft(Option.empty[Chain[B]])(
+      (acc, element) =>
+        Some(acc.fold(Singleton(f(element)): Chain[B])(_ :+ f(element)))
+    ).get // safe, as there is always at least one element
+
+  /*
+  def flatMap[B](f: A => Chain[B]): Chain[B] = this match {
+    case Singleton(element) => f(element)
+    case Append(left, right) => Append(left flatMap f, right flatMap f)
+  }
+   */
+
+  def flatMap[B](f: A => Chain[B]): Chain[B] =
+    foldLeft(Option.empty[Chain[B]])(
+      (acc, element) => Some(acc.fold(f(element))(_ ++ f(element)))
+    ).get // safe, as there is always at least one element
 
   def foreach(f: A => Unit): Unit = foldLeft(())((_, next) => f(next))
 
+  /*
   override def equals(that: Any): Boolean = that match {
-    case c: Chain[_] => ???
+    case c: Chain[_] => toList == c.toList
     case _ => false
+  }
+   */
+
+  override def equals(that: Any): Boolean = {
+    @tailrec
+    def helper[B](one: Chain[A], two: Chain[B]): Boolean = (one, two) match {
+      case (Singleton(a), Singleton(b)) => a == b
+      case (Append(Singleton(a), restA), Append(Singleton(b), restB)) =>
+        a == b && helper(restA, restB)
+      case _ => false
+    }
+
+    that match {
+      case c: Chain[_] => helper(this.listify, c.listify)
+      case _           => false
+    }
   }
 
   override def hashCode: Int = foldLeft(0) {
@@ -37,13 +114,46 @@ sealed trait Chain[+A] {
 
   override def toString: String = toList.mkString("Chain(", ",", ")")
 
-  def toList: List[A] = foldLeft(List.empty[A])((acc, next) => next :: acc).reverse
+  def toList: List[A] =
+    foldLeft(List.empty[A])((acc, next) => next :: acc).reverse
   def toSet[B >: A]: Set[B] = foldLeft(Set.empty[B])((acc, next) => acc + next)
 
-  def min[B >: A](implicit order: Ordering[B]): B = ???
-  def max[B >: A](implicit order: Ordering[B]): B = ???
+  def min[B >: A](implicit order: Ordering[B]): A =
+    reduceLeft(order.min).asInstanceOf[A]
+  def max[B >: A](implicit order: Ordering[B]): A =
+    reduceLeft(order.max).asInstanceOf[A]
 
-  def listify: Chain[A] = ???
+  /*
+  def listify: Chain[A] = foldRight(Option.empty[Chain[A]])((element, acc) =>
+    Some(acc.fold(Singleton(element): Chain[A])(Append(Singleton(element), _)))
+  ).get // safe, as there is always at least one element
+   */
+
+  def listify: Chain[A] = {
+    @tailrec
+    def initial_helper(r: List[Chain[A]], l: List[Chain[A]]): Chain[A] =
+      r match {
+        case (last @ Singleton(_)) :: rs => helper(rs, l, last)
+        case Append(left, right) :: rs   => initial_helper(right :: rs, left :: l)
+        case Nil                         => sys.error("Unexpected format")
+      }
+
+    @tailrec
+    def helper(r: List[Chain[A]], l: List[Chain[A]], acc: Chain[A]): Chain[A] =
+      r match {
+        case Singleton(element) :: rs  => helper(rs, l, element +: acc)
+        case Append(left, right) :: rs => helper(right :: rs, left :: l, acc)
+        case Nil =>
+          l match {
+            case Singleton(element) :: ls => helper(r, ls, element +: acc)
+            case Append(left, right) :: ls =>
+              helper(right :: r, left :: ls, acc)
+            case Nil => acc
+          }
+      }
+
+    initial_helper(List(this), Nil)
+  }
 }
 
 case class Singleton[+A](head: A) extends Chain[A] {
@@ -53,12 +163,14 @@ case class Append[+A](left: Chain[A], right: Chain[A]) extends Chain[A] {
   def head: A = left.head
   def tail: Option[Chain[A]] = left match {
     case Singleton(_) => Some(right)
-    case _ => listify.tail
+    case _            => listify.tail
   }
 }
 
 object Chain {
-  def apply[A](head: A, rest: A*): Chain[A] = ???
+  def apply[A](head: A, rest: A*): Chain[A] =
+    if (rest.isEmpty) Singleton(head)
+    else Append(Singleton(head), Chain(rest.head, rest.tail: _*))
 
   // Allows Chain to be used in pattern matching
   def unapplySeq[A](chain: Chain[A]): Option[Seq[A]] = Some(chain.toList)
--- homework/2-validation/src/test/scala/homework2/ChainTest.scala
+++ homework/2-validation/src/test/scala/homework2/ChainTest.scala
@@ -3,7 +3,189 @@ package homework2
 import org.scalatest.{FlatSpec, Matchers}
 
 class ChainTest extends FlatSpec with Matchers {
-  "++" should "append two chains" in {
+  "Chain apply with one argument" should "create a Singleton chain" in {
+    Chain(42) shouldEqual Singleton(42)
+  }
+
+  "Chain apply with many arguments" should "create an Append chain" in {
+    Chain(1, 2, 3)
+      .shouldEqual(Append(Singleton(1), Append(Singleton(2), Singleton(3))))
+  }
+
+  "head of Singleton" should "return only one element" in {
+    Singleton(42).head shouldEqual 42
+  }
+
+  "head of Append" should "return first element" in {
+    Append(Chain(1, 2), Chain(11, 12)).head shouldEqual 1
+  }
+
+  "tail of Singleton" should "return None" in {
+    Singleton(42).tail shouldEqual None
+  }
+
+  "tail of Append" should "return remainder of chain" in {
+    Append(Chain(1, 2), Chain(11, 12)).tail shouldEqual Some(Chain(2, 11, 12))
+  }
+
+  "isEmpty of Singleton" should "return false" in {
+    Singleton(42).isEmpty shouldEqual false
+  }
+
+  "isEmpty of Append" should "return false" in {
+    Append(Chain(1, 2), Chain(11, 12)).isEmpty shouldEqual false
+  }
+
+  "+: of Singleton" should "add an element in front" in {
+    24 +: Singleton(42) shouldEqual Chain(24, 42)
+  }
+
+  "+: of Append" should "add an element in front" in {
+    24 +: Append(Chain(1, 2), Chain(11, 12)) shouldEqual
+      Chain(24, 1, 2, 11, 12)
+  }
+
+  ":+ of Singleton" should "add an element in front" in {
+    Singleton(42) :+ 24 shouldEqual Chain(42, 24)
+  }
+
+  ":+ of Append" should "add an element in front" in {
+    Append(Chain(1, 2), Chain(11, 12)) :+ 24 shouldEqual
+      Chain(1, 2, 11, 12, 24)
+  }
+
+  "++ of two Singletons" should "append two them" in {
+    (Singleton(24) ++ Singleton(42)) shouldEqual Chain(24, 42)
+  }
+
+  "++ of two chains" should "append them" in {
     (Chain(1, 2) ++ Chain(3, 4)) shouldEqual Chain(1, 2, 3, 4)
+  }
+
+  "foldLeft of Singleton" should "iterate once" in {
+    Singleton(42).foldLeft(0)(_ - _) shouldEqual -42
+    Singleton(2.0).foldLeft(1.0)(_ / _) shouldEqual 0.5
+  }
+
+  "foldLeft of balanced Append" should "iterate properly" in {
+    Append(Chain(1, 2), Chain(11, 12)).foldLeft(0)(_ - _) shouldEqual -26
+    Append(Chain(2.0, 4.0), Chain(8.0, 16.0))
+      .foldLeft(512.0)(_ / _)
+      .shouldEqual(0.5)
+  }
+
+  "foldLeft of left-weighted Append" should "iterate properly" in {
+    Append(Chain(1), Append(Chain(2), Chain(11, 12)))
+      .foldLeft(0)(_ - _)
+      .shouldEqual(-26)
+    Append(Chain(2.0), Append(Chain(4.0), Chain(8.0, 16.0)))
+      .foldLeft(512.0)(_ / _) shouldEqual (0.5)
+  }
+
+  "foldLeft of right-weighted Append" should "iterate properly" in {
+    Append(Append(Chain(1, 2), Chain(11)), Chain(12))
+      .foldLeft(0)(_ - _)
+      .shouldEqual(-26)
+    Append(Append(Chain(2.0, 4.0), Chain(8.0)), Chain(16.0))
+      .foldLeft(512.0)(_ / _)
+      .shouldEqual(0.5)
+  }
+
+  "reduceLeft of Singleton" should "not iterate" in {
+    Singleton(42).reduceLeft((_, _) => 0) shouldEqual 42
+  }
+
+  "reduceLeft of Append" should "iterate properly" in {
+    Append(Chain(1, 2), Chain(11, 12)).reduceLeft(_ + _) shouldEqual 26
+    Append(Chain(1, 2), Chain(11, 12)).reduceLeft(_ * _) shouldEqual 264
+  }
+
+  "map of Singleton" should "map properly" in {
+    Singleton(42) map (_ * 2) shouldEqual Singleton(84)
+  }
+
+  "map of Append" should "map properly" in {
+    Append(Chain(1, 2), Chain(11, 12))
+      .map(_ * 2)
+      .shouldEqual(Append(Chain(2, 4), Chain(22, 24)))
+  }
+
+  "flatMap of Singleton" should "flatMap properly" in {
+    Singleton(List(4, 2))
+      .flatMap(l => Chain(l.head, l.tail: _*))
+      .shouldEqual(Chain(4, 2))
+  }
+
+  "flatMap of Append" should "flatMap properly" in {
+    Append(Chain(List(1, 2), List(11)), Chain(List(21, 22), List(31, 32, 33)))
+      .flatMap(l => Chain(l.head, l.tail: _*))
+      .shouldEqual(
+        Append(
+          Append(Chain(1, 2), Chain(11)),
+          Append(Chain(21, 22), Chain(31, 32, 33))
+        )
+      )
+  }
+
+  "foreach of Singleton" should "iterate properly" in {
+    val buffer = scala.collection.mutable.ListBuffer.empty[Int]
+    Singleton(42).foreach(buffer append _)
+    buffer.toList shouldEqual List(42)
+  }
+
+  "foreach of Append" should "iterate properly" in {
+    val buffer = scala.collection.mutable.ListBuffer.empty[Int]
+    Append(Chain(1, 2), Chain(11, 12)).foreach(buffer append _)
+    buffer.toList shouldEqual List(1, 2, 11, 12)
+  }
+
+  "equals of Singleton" should "compare equality" in {
+    Singleton(42) == Singleton(42) shouldBe true
+    Singleton(42) == Singleton(24) shouldBe false
+  }
+
+  "equals of Append" should "compare equality" in {
+    (Append(Chain(1, 2), Chain(11, 12)) == Append(Chain(1, 2), Chain(11, 12)))
+      .shouldBe(true)
+    (Append(Chain(1, 2), Chain(11, 12)) == Append(Chain(10, 2), Chain(11, 12)))
+      .shouldBe(false)
+  }
+
+  "equals of mixed chains" should "compare equality" in {
+    Append(Chain(1, 2), Chain(11, 12)) == Chain(1, 2, 11, 12) shouldBe true
+    Singleton(42) == Chain(42) shouldBe true
+    Singleton(42) == Append(Chain(1, 2), Chain(11, 12)) shouldBe false
+  }
+
+  "toList of Singleton" should "convert properly" in {
+    Singleton(42).toList shouldEqual List(42)
+  }
+
+  "toList of Append" should "convert properly" in {
+    Append(Chain(1, 2), Chain(11, 12)).toList shouldEqual List(1, 2, 11, 12)
+  }
+
+  "toSet of Singleton" should "convert properly" in {
+    Singleton(42).toSet shouldEqual Set(42)
+  }
+
+  "toSet of Append" should "convert properly" in {
+    Append(Chain(1, 12), Chain(12, 11, 12)).toSet shouldEqual Set(1, 11, 12)
+  }
+
+  "min of Singleton" should "order properly" in {
+    Singleton(42).min shouldEqual 42
+  }
+
+  "min of Append" should "order properly" in {
+    Append(Chain(1, 100, 2), Chain(11, -5, 12)).min shouldEqual -5
+  }
+
+  "max of Singleton" should "order properly" in {
+    Singleton(42).max shouldEqual 42
+  }
+
+  "max of Append" should "order properly" in {
+    Append(Chain(1, 100, 2), Chain(11, -5, 12)).max shouldEqual 100
   }
 }
