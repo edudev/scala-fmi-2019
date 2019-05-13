3fa6d8a808da4f45ed7102be09c1f29c17b29137
Emil Dudev <emildudev@gmail.com>
2019-05-13 23:16:34 +0000

    Implement Validated

--- homework/2-validation/src/main/scala/homework2/Validated.scala
+++ homework/2-validation/src/main/scala/homework2/Validated.scala
@@ -1,29 +1,52 @@
 package homework2
 
 sealed trait Validated[+E, +A] {
-  def isValid: Boolean = ???
+  def isValid: Boolean = this match {
+    case Valid(_)   => true
+    case Invalid(_) => false
+  }
 
-  def getOrElse[B >: A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Valid(a) => a
+    case _        => default
+  }
 
-  def orElse[F >: E, B >: A](default: => Validated[F, B]): Validated[F, B] = ???
+  def orElse[F >: E, B >: A](default: => Validated[F, B]): Validated[F, B] =
+    if (isValid) this else default
 
-  def zip[EE >: E, B](vb: Validated[EE, B]): Validated[EE, (A, B)] = ???
+  def zip[EE >: E, B](vb: Validated[EE, B]): Validated[EE, (A, B)] =
+    (this, vb) match {
+      case (Valid(a), Valid(b))      => Valid((a, b))
+      case (Invalid(a), Invalid(b))  => Invalid(a ++ b)
+      case (invalid: Invalid[EE], _) => invalid
+      case (_, invalid: Invalid[EE]) => invalid
+    }
 
-  def map[B](f: A => B): Validated[E, B] = ???
+  def map[B](f: A => B): Validated[E, B] = this match {
+    case Valid(a)            => Valid(f(a))
+    case invalid: Invalid[E] => invalid
+  }
 
-  def map2[EE >: E, B, R](vb: Validated[EE, B])(f: (A, B) => R): Validated[EE, R] = ???
+  def map2[EE >: E, B, R](
+      vb: Validated[EE, B]
+  )(f: (A, B) => R): Validated[EE, R] =
+    zip(vb).map(f.tupled)
 
-  def flatMap[EE >: E, B](f: A => Validated[EE, B]): Validated[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Validated[EE, B]): Validated[EE, B] =
+    this match {
+      case Valid(a)            => f(a)
+      case invalid: Invalid[E] => invalid
+    }
 
   def fold[B](invalid: Chain[E] => B, valid: A => B): B = this match {
     case Invalid(errors) => invalid(errors)
-    case Valid(a) => valid(a)
+    case Valid(a)        => valid(a)
   }
 
   def foreach(f: A => Unit): Unit = fold(_ => (), f)
 }
 
-case class Valid[+A](a: A) extends Validated[Nothing, A]
+case class Valid[+A](a: A)               extends Validated[Nothing, A]
 case class Invalid[+E](errors: Chain[E]) extends Validated[E, Nothing]
 
 object Invalid {
@@ -31,29 +54,62 @@ object Invalid {
 }
 
 object Validated {
-  def sequence[E, A](xs: List[Validated[E, A]]): Validated[E, List[A]] = ???
+  def sequence[E, A](xs: List[Validated[E, A]]): Validated[E, List[A]] =
+    xs.foldRight(Valid(List.empty[A]): Validated[E, List[A]])(
+      _.map2(_)(_ :: _)
+    )
 
-  implicit class ValidatedTuple2[EE, A, B](val tuple: (Validated[EE, A], Validated[EE, B])) extends AnyVal {
-    def zip: Validated[EE, (A, B)] = ???
-    def zipMap[R](f: (A, B) => R): Validated[EE, R] = ???
+  implicit class ValidatedTuple2[EE, A, B](
+      val tuple: (Validated[EE, A], Validated[EE, B])
+  ) extends AnyVal {
+    def zip: Validated[EE, (A, B)]                  = tuple._1 zip tuple._2
+    def zipMap[R](f: (A, B) => R): Validated[EE, R] = tuple.zip.map(f.tupled)
   }
 
-  implicit class ValidatedTuple3[EE, A, B, C](val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C])) extends AnyVal {
-    def zip: Validated[EE, (A, B, C)] = ???
-    def zipMap[R](f: (A, B, C) => R): Validated[EE, R] = ???
+  implicit class ValidatedTuple3[EE, A, B, C](
+      val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C])
+  ) extends AnyVal {
+    def zip: Validated[EE, (A, B, C)] =
+      (tuple._1, tuple._2).zip.map2(tuple._3)((ab, c) => (ab._1, ab._2, c))
+    def zipMap[R](f: (A, B, C) => R): Validated[EE, R] =
+      tuple.zip.map(f.tupled)
   }
 
-  implicit class ValidatedTuple4[EE, A, B, C, D]
-  (val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C], Validated[EE, D])) extends AnyVal {
-    def zip: Validated[EE, (A, B, C, D)] = ???
-    def zipMap[R](f: (A, B, C, D) => R): Validated[EE, R] = ???
+  implicit class ValidatedTuple4[EE, A, B, C, D](
+      val tuple: (
+          Validated[EE, A],
+          Validated[EE, B],
+          Validated[EE, C],
+          Validated[EE, D]
+      )
+  ) extends AnyVal {
+    def zip: Validated[EE, (A, B, C, D)] =
+      (tuple._1, tuple._2, tuple._3).zip.map2(tuple._4)(
+        (abc, d) => (abc._1, abc._2, abc._3, d)
+      )
+    def zipMap[R](f: (A, B, C, D) => R): Validated[EE, R] =
+      tuple.zip.map(f.tupled)
   }
 
-  implicit class ValidatedTuple5[EE, A, B, C, D, E]
-  (val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C], Validated[EE, D], Validated[EE, E])) extends AnyVal {
-    def zip: Validated[EE, (A, B, C, D, E)] = ???
-    def zipMap[R](f: (A, B, C, D, E) => R): Validated[EE, R] = ???
+  implicit class ValidatedTuple5[EE, A, B, C, D, E](
+      val tuple: (
+          Validated[EE, A],
+          Validated[EE, B],
+          Validated[EE, C],
+          Validated[EE, D],
+          Validated[EE, E]
+      )
+  ) extends AnyVal {
+    def zip: Validated[EE, (A, B, C, D, E)] =
+      (tuple._1, tuple._2, tuple._3, tuple._4).zip.map2(tuple._5)(
+        (abcd, e) => (abcd._1, abcd._2, abcd._3, abcd._4, e)
+      )
+    def zipMap[R](f: (A, B, C, D, E) => R): Validated[EE, R] =
+      tuple.zip.map(f.tupled)
   }
 
-  // ??? TODO: Add toValidated to Option instances
+  implicit class ValidatedOption[+A](val option: Option[A]) extends AnyVal {
+    def toValidated[E](onEmpty: => E): Validated[E, A] =
+      option.fold(Invalid(onEmpty): Validated[E, A])(Valid(_))
+  }
 }
--- homework/2-validation/src/test/scala/homework2/ValidatedTest.scala
+++ homework/2-validation/src/test/scala/homework2/ValidatedTest.scala
@@ -3,11 +3,290 @@ package homework2
 import org.scalatest.{FlatSpec, FunSuite, Matchers}
 
 class ValidatedTest extends FlatSpec with Matchers {
+  "isValid of Valid" should "be always true" in {
+    Valid(1).isValid shouldEqual true
+    Valid(false).isValid shouldEqual true
+  }
+
+  "isValid of Invalid" should "be always false" in {
+    Invalid("").isValid shouldEqual false
+    Invalid(true).isValid shouldEqual false
+  }
+
+  "getOrElse of Valid" should "be non-strict" in {
+    Valid(42).getOrElse(Stream.from(1).toList) shouldEqual 42
+  }
+
+  "getOrElse of Invalid" should "return the default value" in {
+    Invalid(42).getOrElse(24) shouldEqual 24
+  }
+
+  "orElse of Valid" should "be non-strict" in {
+    Valid(42).orElse(Valid(Stream.from(1).toList)) shouldEqual Valid(42)
+    Valid(42).orElse(Invalid(Stream.from(1).toList)) shouldEqual Valid(42)
+  }
+
+  "orElse of Invalid" should "return the default value" in {
+    Invalid(42).orElse(Valid(24)) shouldEqual Valid(24)
+    Invalid(42).orElse(Invalid(24)) shouldEqual Invalid(24)
+  }
+
   "zip" should "combine valid instances" in {
     Valid(1).zip(Valid("a")) shouldEqual Valid((1, "a"))
   }
 
   it should "combine errors from invalid instances" in {
     Invalid(1).zip(Invalid(Chain(2, 3))) shouldEqual Invalid(Chain(1, 2, 3))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    Valid(1).zip(Invalid(Chain(2, 3))) shouldEqual Invalid(Chain(2, 3))
+    Invalid(1).zip(Valid(23)) shouldEqual Invalid(1)
+  }
+
+  "map of Valid" should "transform the value" in {
+    Valid(42).map(_ => 24) shouldEqual Valid(24)
+  }
+
+  "map of Invalid" should "not change the error" in {
+    Invalid(42).map(_ => 24) shouldEqual Invalid(42)
+  }
+
+  "map2" should "combine valid instances and transform them" in {
+    Valid(-5).map2(Valid(42))(_ + _) shouldEqual Valid(37)
+  }
+
+  it should "combine errors from invalid instances" in {
+    Invalid(1)
+      .map2(Invalid(Chain(2, 3)))((_, _) => 24)
+      .shouldEqual(Invalid(Chain(1, 2, 3)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    Valid(1)
+      .map2(Invalid(Chain(2, 3)))((_, _) => 24)
+      .shouldEqual(Invalid(Chain(2, 3)))
+
+    Invalid(1).map2(Valid(23))((_, _) => 24) shouldEqual Invalid(1)
+  }
+
+  "flatMap of Valid" should "transform the value" in {
+    Valid(42).flatMap(_ => Valid(24)) shouldEqual Valid(24)
+    Valid(42).flatMap(_ => Invalid(24)) shouldEqual Invalid(24)
+  }
+
+  "flatMap of Invalid" should "preserve the instance" in {
+    Invalid(42).flatMap(_ => Valid(24)) shouldEqual Invalid(42)
+    Invalid(42).flatMap(_ => Invalid(24)) shouldEqual Invalid(42)
+  }
+
+  "fold of Valid" should "transform the value" in {
+    Valid(42).fold(_ => Stream.from(1).toList, _ * 2) shouldEqual 84
+  }
+
+  "fold of Invalid" should "transform the errors" in {
+    Invalid(42)
+      .fold(_.toList, _ => Stream.from(1).toList)
+      .shouldEqual(List(42))
+  }
+
+  "foreach of Valid" should "call the function" in {
+    val buffer = scala.collection.mutable.ListBuffer.empty[Int]
+    Valid(42).foreach(buffer append _)
+    buffer.toList shouldEqual List(42)
+  }
+
+  "foreach of Invalid" should "not call anything" in {
+    val buffer = scala.collection.mutable.ListBuffer.empty[Int]
+    Invalid(42).foreach(buffer append _)
+    buffer.toList shouldEqual List()
+  }
+
+  "Tuple2 zip" should "combine valid instances" in {
+    (Valid(1), Valid("a")).zip shouldBe Valid((1, "a"))
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1.0), Invalid(Chain(2, 3))).zip shouldBe Invalid(Chain(1.0, 2, 3))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(Chain(2, 3))).zip shouldBe Invalid(Chain(2, 3))
+    (Invalid(1), Valid(23)).zip shouldBe Invalid(1)
+  }
+
+  "Tuple2 zipMap" should "combine valid instances and transform them" in {
+    (Valid(-5), Valid(42)).zipMap(_ + _) shouldBe Valid(37)
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(Chain(2, 3)))
+      .zipMap((_: Nothing, _: Nothing) => 24)
+      .shouldBe(Invalid(Chain(1, 2, 3)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(Chain(2, 3)))
+      .zipMap((_, _: Nothing) => 24)
+      .shouldBe(Invalid(Chain(2, 3)))
+
+    (Invalid(1), Valid(23)).zipMap((_: Nothing, _) => 24) shouldBe Invalid(1)
+  }
+
+  "Tuple3 zip" should "combine valid instances" in {
+    (Valid(1), Valid(2), Valid(3)).zip shouldBe Valid((1, 2, 3))
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(2), Invalid(3)).zip shouldBe Invalid(Chain(1, 2, 3))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(2), Valid(3)).zip shouldBe Invalid(2)
+    (Invalid(1), Valid(2), Invalid(3)).zip shouldBe Invalid(Chain(1, 3))
+  }
+
+  "Tuple3 zipMap" should "combine valid instances and transform them" in {
+    (Valid(1), Valid(2), Valid(3)).zipMap(_ + _ + _) shouldBe Valid(6)
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(2), Invalid(3))
+      .zipMap((_: Nothing, _: Nothing, _: Nothing) => 24)
+      .shouldBe(Invalid(Chain(1, 2, 3)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(2), Valid(3))
+      .zipMap((_, _: Nothing, _) => 24)
+      .shouldBe(Invalid(2))
+
+    (Invalid(1), Valid(2), Invalid(3))
+      .zipMap((_: Nothing, _, _: Nothing) => 42)
+      .shouldBe(Invalid(Chain(1, 3)))
+  }
+
+  "Tuple4 zip" should "combine valid instances" in {
+    (Valid(1), Valid(2), Valid(3), Valid(4)).zip shouldBe Valid((1, 2, 3, 4))
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(2), Invalid(3), Invalid(4)).zip
+      .shouldBe(Invalid(Chain(1, 2, 3, 4)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(2), Valid(3), Valid(4)).zip shouldBe Invalid(2)
+    (Invalid(1), Valid(2), Invalid(3), Invalid(4)).zip
+      .shouldBe(Invalid(Chain(1, 3, 4)))
+  }
+
+  "Tuple4 zipMap" should "combine valid instances and transform them" in {
+    (Valid(1), Valid(2), Valid(3), Valid(4))
+      .zipMap(_ + _ + _ + _)
+      .shouldBe(Valid(10))
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(2), Invalid(3), Invalid(4))
+      .zipMap((_: Nothing, _: Nothing, _: Nothing, _: Nothing) => 24)
+      .shouldBe(Invalid(Chain(1, 2, 3, 4)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(2), Valid(3), Valid(4))
+      .zipMap((_, _: Nothing, _, _) => 24)
+      .shouldBe(Invalid(2))
+
+    (Invalid(1), Valid(2), Invalid(3), Invalid(4))
+      .zipMap((_: Nothing, _, _: Nothing, _: Nothing) => 42)
+      .shouldBe(Invalid(Chain(1, 3, 4)))
+  }
+
+  "Tuple5 zip" should "combine valid instances" in {
+    (Valid(1), Valid(2), Valid(3), Valid(4), Valid(5)).zip
+      .shouldBe(Valid((1, 2, 3, 4, 5)))
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(2), Invalid(3), Invalid(4), Invalid(5)).zip
+      .shouldBe(Invalid(Chain(1, 2, 3, 4, 5)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(2), Valid(3), Valid(4), Valid(5)).zip
+      .shouldBe(Invalid(2))
+    (Invalid(1), Valid(2), Invalid(3), Invalid(4), Invalid(5)).zip
+      .shouldBe(Invalid(Chain(1, 3, 4, 5)))
+  }
+
+  "Tuple5 zipMap" should "combine valid instances and transform them" in {
+    (Valid(1), Valid(2), Valid(3), Valid(4), Valid(5))
+      .zipMap(_ + _ + _ + _ + _)
+      .shouldBe(Valid(15))
+  }
+
+  it should "combine errors from invalid instances" in {
+    (Invalid(1), Invalid(2), Invalid(3), Invalid(4), Invalid(5))
+      .zipMap(
+        (_: Nothing, _: Nothing, _: Nothing, _: Nothing, _: Nothing) => 24
+      )
+      .shouldBe(Invalid(Chain(1, 2, 3, 4, 5)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    (Valid(1), Invalid(2), Valid(3), Valid(4), Valid(5))
+      .zipMap((_, _: Nothing, _, _, _) => 24)
+      .shouldBe(Invalid(2))
+
+    (Invalid(1), Valid(2), Invalid(3), Invalid(4), Invalid(5))
+      .zipMap((_: Nothing, _, _: Nothing, _: Nothing, _: Nothing) => 42)
+      .shouldBe(Invalid(Chain(1, 3, 4, 5)))
+  }
+
+  "sequence" should "combine valid instances" in {
+    val range = 1 to 20
+    Validated.sequence(range.map(Valid(_)).toList) shouldBe Valid(range.toList)
+  }
+
+  it should "combine errors from invalid instances" in {
+    val range = 1 to 20
+    Validated
+      .sequence(range.map(Invalid(_)).toList)
+      .shouldBe(Invalid(Chain(range.head, range.tail: _*)))
+  }
+
+  it should "combine errors from at least one invalid instance" in {
+    val source1 = (1 to 20)
+      .map(Valid(_))
+      .toList
+      .updated(1, Invalid(2))
+      .updated(13, Invalid(41))
+
+    Validated.sequence(source1) shouldBe Invalid(Chain(2, 41))
+
+    val source2 = (1 to 20)
+      .map(Invalid(_))
+      .toList
+      .updated(1, Valid(2))
+      .updated(13, Valid(41))
+
+    Validated
+      .sequence(source2)
+      .shouldBe(
+        Invalid(
+          Chain(1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20)
+        )
+      )
+  }
+
+  "toValidated of None" should "return the invalid value" in {
+    Option.empty[Valid[Int]].toValidated(42) shouldEqual Invalid(42)
+  }
+
+  "toValidated of Some" should "return the option valid as valid" in {
+    Some(Invalid(1))
+      .toValidated(Stream.from(1).toList)
+      .shouldEqual(Valid(Invalid(1)))
   }
 }
