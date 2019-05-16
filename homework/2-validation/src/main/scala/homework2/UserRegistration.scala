036a355f09547b1c768d207752395ef0d0bd5a7c
Emil Dudev <emildudev@gmail.com>
2019-05-16 18:05:06 +0000

    Implement UserRegistration

--- homework/2-validation/src/main/scala/homework2/UserRegistration.scala
+++ homework/2-validation/src/main/scala/homework2/UserRegistration.scala
@@ -1,13 +1,15 @@
 package homework2
 
-case class RegistrationForm(name: String,
-                            email: String,
-                            password: String,
-                            passwordConfirmation: String,
-                            birthYear: String,
-                            birthMonth: String,
-                            birthDay: String,
-                            postalCode: String)
+case class RegistrationForm(
+    name: String,
+    email: String,
+    password: String,
+    passwordConfirmation: String,
+    birthYear: String,
+    birthMonth: String,
+    birthDay: String,
+    postalCode: String
+)
 
 sealed trait RegistrationFormError
 
@@ -15,32 +17,150 @@ case object NameIsEmpty extends RegistrationFormError
 
 case class InvalidEmail(email: String) extends RegistrationFormError
 
-case object PasswordTooShort extends RegistrationFormError
+case object PasswordTooShort                     extends RegistrationFormError
 case object PasswordRequiresGreaterSymbolVariety extends RegistrationFormError
-case object PasswordsDoNotMatch extends RegistrationFormError
+case object PasswordsDoNotMatch                  extends RegistrationFormError
 
-case class InvalidBirthdayDate(dateErrors: Chain[DateError]) extends RegistrationFormError
+case class InvalidBirthdayDate(dateErrors: Chain[DateError])
+    extends RegistrationFormError
 case class BirthdayDateIsInTheFuture(date: Date) extends RegistrationFormError
 
 case class InvalidPostalCode(code: String) extends RegistrationFormError
 
 sealed trait DateError
-case class YearIsNotAnInteger(year: String) extends DateError
+case class YearIsNotAnInteger(year: String)   extends DateError
 case class MonthIsNotAnInteger(month: String) extends DateError
-case class DayIsNotAnInteger(day: String) extends DateError
-case class MonthOutOfRange(month: Int) extends DateError
-case class DayOutOfRange(day: Int) extends DateError
-case class InvalidDate(date: Date) extends DateError
+case class DayIsNotAnInteger(day: String)     extends DateError
+case class MonthOutOfRange(month: Int)        extends DateError
+case class DayOutOfRange(day: Int)            extends DateError
+case class InvalidDate(date: Date)            extends DateError
 
 case class Email(user: String, domain: String)
 
-case class User(name: String,
-                email: Email,
-                passwordHash: String,
-                birthday: Date,
-                postalCode: Option[String])
+case class User(
+    name: String,
+    email: Email,
+    passwordHash: String,
+    birthday: Date,
+    postalCode: Option[String]
+)
 
 object UserRegistration {
-  def registerUser(userCountryPostalCodeVerifier: String => Boolean, today: Date)
-                  (form: RegistrationForm): Validated[RegistrationFormError, User] = ???
+  def registerUser(
+      userCountryPostalCodeVerifier: String => Boolean,
+      today: Date
+  )(form: RegistrationForm): Validated[RegistrationFormError, User] =
+    (
+      validateName(form.name),
+      validateEmail(form.email),
+      validatePassword(form.password, form.passwordConfirmation),
+      validateDate(form.birthYear, form.birthMonth, form.birthDay)
+        .fold(
+          errors => Invalid(InvalidBirthdayDate(errors)),
+          validateBirtday(_, today)
+        ),
+      validatePostalCode(form.postalCode, userCountryPostalCodeVerifier)
+    ).zipMap(User.apply)
+
+  def validateDate(
+      year: String,
+      month: String,
+      day: String
+  ): Validated[DateError, Date] = {
+    import Validated._
+
+    def toInteger(string: String): Option[Int] =
+      try {
+        Some(string.toInt)
+      } catch {
+        case _: NumberFormatException => None
+      }
+
+    def validateInteger(
+        string: String,
+        error: => DateError
+    ): Validated[DateError, Int] =
+      toInteger(string).toValidated(error)
+
+    def boundCheck(
+        number: Int,
+        min: Int,
+        max: Int,
+        error: => DateError
+    ): Validated[DateError, Int] =
+      if ((min to max).contains(number)) Valid(number) else Invalid(error)
+
+    def validityCheck(date: Date): Validated[DateError, Date] =
+      Date
+        .applyOption(date.year, date.month, date.day)
+        .toValidated(InvalidDate(date))
+
+    (
+      validateInteger(year, YearIsNotAnInteger(year)),
+      validateInteger(month, MonthIsNotAnInteger(month))
+        .flatMap(month => boundCheck(month, 1, 12, MonthOutOfRange(month))),
+      validateInteger(day, DayIsNotAnInteger(day))
+        .flatMap(day => boundCheck(day, 1, 31, DayOutOfRange(day)))
+    ).zipMap(Date.apply).flatMap(validityCheck)
+  }
+
+  private def validateName(
+      name: String
+  ): Validated[RegistrationFormError, String] =
+    if (name.isEmpty) Invalid(NameIsEmpty) else Valid(name)
+
+  private def validateEmail(
+      email: String
+  ): Validated[RegistrationFormError, Email] =
+    email.split('@') match {
+      case Array(user, domain) if !user.isEmpty && !domain.isEmpty =>
+        Valid(Email(user, domain))
+      case _ => Invalid(InvalidEmail(email))
+    }
+
+  private def validatePassword(
+      password: String,
+      passwordConfirmation: String
+  ): Validated[RegistrationFormError, String] = {
+    val numbers = '0' to '9'
+    val letters = ('A' to 'Z') ++ ('a' to 'z')
+
+    def highPasswordVariety(password: String): Boolean =
+      password.exists(numbers.contains(_)) &&
+        password.exists(letters.contains(_)) &&
+        !password.forall(c => letters.contains(c) || numbers.contains(c))
+
+    (
+      if (password.length >= 8) Valid(password) else Invalid(PasswordTooShort),
+      if (highPasswordVariety(password)) Valid(password)
+      else Invalid(PasswordRequiresGreaterSymbolVariety),
+      if (password == passwordConfirmation) Valid(password)
+      else Invalid(PasswordsDoNotMatch)
+    ).zipMap((password, _, _) => PasswordUtils.hash(password))
+  }
+
+  private def validateBirtday(
+      birthday: Date,
+      today: Date
+  ): Validated[RegistrationFormError, Date] = {
+    import scala.math.Ordered.orderingToOrdered
+    def compareDates(a: Date, b: Date): Int =
+      (a.year, a.month, a.day) compare (b.year, b.month, b.day)
+
+    if (compareDates(birthday, today) <= 0)
+      Valid(birthday)
+    else
+      Invalid(BirthdayDateIsInTheFuture(birthday))
+  }
+
+  private def validatePostalCode(
+      postalCode: String,
+      userCountryPostalCodeVerifier: String => Boolean
+  ): Validated[RegistrationFormError, Option[String]] =
+    if (postalCode.isEmpty)
+      Valid(None)
+    else if (userCountryPostalCodeVerifier(postalCode))
+      Valid(Some(postalCode))
+    else
+      Invalid(InvalidPostalCode(postalCode))
 }
--- homework/2-validation/src/test/scala/homework2/UserRegistrationTest.scala
+++ homework/2-validation/src/test/scala/homework2/UserRegistrationTest.scala
@@ -12,7 +12,7 @@ class UserRegistrationTest extends FlatSpec with Matchers {
     validation.isValid shouldBe false
 
     val Invalid(errors) = validation
-    val errorsSet = errors.toSet
+    val errorsSet       = errors.toSet
     val birthdayErrors = errorsSet.collectFirst {
       case InvalidBirthdayDate(dateErrors) => dateErrors.toSet
     }
@@ -26,10 +26,435 @@ class UserRegistrationTest extends FlatSpec with Matchers {
       PasswordRequiresGreaterSymbolVariety
     )
 
-    birthdayErrors shouldEqual Some(Set(
-      YearIsNotAnInteger(""),
-      MonthIsNotAnInteger(""),
-      DayIsNotAnInteger("")
-    ))
+    birthdayErrors shouldEqual Some(
+      Set(
+        YearIsNotAnInteger(""),
+        MonthIsNotAnInteger(""),
+        DayIsNotAnInteger("")
+      )
+    )
+  }
+
+  "A correct form" should "generate no errors and a valid User" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16",
+      "1000"
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+
+    validation.isValid shouldBe true
+
+    val Valid(user) = validation
+
+    user.name shouldBe "ivan"
+    user.email shouldBe Email("ivan", "mail.bg")
+    user.passwordHash should not be "passw0rd!"
+    user.birthday shouldBe Date(2019, 5, 16)
+    user.postalCode shouldBe Some("1000")
+  }
+
+  "A correct form" should "generate no errors and a valid User with optional postal code" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2009",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+
+    validation.isValid shouldBe true
+
+    val Valid(user) = validation
+
+    user.name shouldBe "ivan"
+    user.email shouldBe Email("ivan", "mail.bg")
+    user.passwordHash should not be "passw0rd!"
+    user.birthday shouldBe Date(2009, 5, 16)
+    user.postalCode shouldBe None
+  }
+
+  "A valid form" should "not have an empty name" in {
+    val form = RegistrationForm(
+      "",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(NameIsEmpty)
+  }
+
+  it should "have a valid email with domain" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(InvalidEmail("ivan@"))
+  }
+
+  it should "have a valid email with user" in {
+    val form = RegistrationForm(
+      "ivan",
+      "@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(InvalidEmail("@mail.bg"))
+  }
+
+  it should "have a valid email with at sign" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan at mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(InvalidEmail("ivan at mail.bg"))
+  }
+
+  it should "have a long password" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "p0!",
+      "p0!",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(PasswordTooShort)
+  }
+
+  it should "have a strong password symbol variety" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "12345678",
+      "12345678",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(PasswordRequiresGreaterSymbolVariety)
+  }
+
+  it should "have a matching password confirmation" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "Passw0rd!",
+      "2019",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(PasswordsDoNotMatch)
+  }
+
+  it should "have a valid postal code" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16",
+      "AAAA"
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(InvalidPostalCode("AAAA"))
+  }
+
+  it should "have an integer birth year" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2K19",
+      "05",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    val error = YearIsNotAnInteger("2K19")
+    errorsSet should contain(InvalidBirthdayDate(Chain(error)))
+  }
+
+  it should "have an integer birth month" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "five",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    val error = MonthIsNotAnInteger("five")
+    errorsSet should contain(InvalidBirthdayDate(Chain(error)))
+  }
+
+  it should "have an integer birth day" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "16th",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    val error = DayIsNotAnInteger("16th")
+    errorsSet should contain(InvalidBirthdayDate(Chain(error)))
+  }
+
+  it should "have a valid birth month" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "13",
+      "16",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    val error = MonthOutOfRange(13)
+    errorsSet should contain(InvalidBirthdayDate(Chain(error)))
+  }
+
+  it should "have a valid birth day" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "05",
+      "36",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    val error = DayOutOfRange(36)
+    errorsSet should contain(InvalidBirthdayDate(Chain(error)))
+  }
+
+  it should "have a valid birthday date" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "02",
+      "31",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    val error = InvalidDate(Date(2019, 2, 31))
+    errorsSet should contain(InvalidBirthdayDate(Chain(error)))
+  }
+
+  it should "have a valid birthday date in the past" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan@mail.bg",
+      "passw0rd!",
+      "passw0rd!",
+      "2019",
+      "5",
+      "31",
+      ""
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 1
+    errorsSet should contain(BirthdayDateIsInTheFuture(Date(2019, 5, 31)))
+  }
+
+  "An invalid form" should "should return many errors" in {
+    val form = RegistrationForm(
+      "ivan",
+      "ivan",
+      "passw0rd!",
+      "passw0rd!",
+      "2K19",
+      "two",
+      "36",
+      "AAA"
+    )
+
+    val validation = registerUser(Set("1000"), Date(2019, 5, 16))(form)
+    validation.isValid shouldBe false
+
+    val Invalid(errors) = validation
+    val errorsSet       = errors.toSet
+
+    errorsSet should have size 3
+    errorsSet should contain allOf (
+      InvalidEmail("ivan"),
+      InvalidPostalCode("AAA")
+    )
+
+    val birthdayErrors = errorsSet.collectFirst {
+      case InvalidBirthdayDate(dateErrors) => dateErrors.toSet
+    }
+
+    birthdayErrors shouldEqual Some(
+      Set(
+        YearIsNotAnInteger("2K19"),
+        MonthIsNotAnInteger("two"),
+        DayOutOfRange(36)
+      )
+    )
   }
 }
